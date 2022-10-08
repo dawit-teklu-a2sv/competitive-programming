fab504eb6c9e0ee115eaa9d400c8f8dd983e142b
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2022-10-08 12:49:26 +0300

    Time: 493 ms (35.23%), Space: 123 MB (98.25%) - LeetHub

---
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& A) {
+        sort(A.begin(), A.end());
+        for (int i = 1; i < A.size(); i += 2)
+            swap(A[i], A[i - 1]);
+        return A;
+    }
+};
