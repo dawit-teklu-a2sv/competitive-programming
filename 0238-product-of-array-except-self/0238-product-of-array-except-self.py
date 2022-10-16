55997bb1c20d917a6879d4d38d01f5a5cb61f5a8
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2022-10-16 15:13:26 +0300

    Time: 302 ms (77.71%), Space: 21.3 MB (58.73%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,22 +1,33 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix_array = []        
-        postfix_array = [1]  * len(nums)
-        output = []
-        product = 1
-        for i in nums:
-            product *= i
-            prefix_array.append(product)
-        product = 1
-        for i in range(len(nums)-1,-1,-1):
-            product *= nums[i]
-            postfix_array[i] = product
+        # prefix_array = []        
+        # postfix_array = [1]  * len(nums)
+        # output = []
+        # product = 1
+        # for i in nums:
+        #     product *= i
+        #     prefix_array.append(product)
+        # product = 1
+        # for i in range(len(nums)-1,-1,-1):
+        #     product *= nums[i]
+        #     postfix_array[i] = product
+        # for i in range(len(nums)):
+        #     if i == 0:
+        #         output.append(postfix_array[1])
+        #     elif i == len(nums)-1:
+        #         output.append(prefix_array[i- 1])
+        #     else:
+        #         output.append(prefix_array[i-1] * postfix_array[i+1])
+        # return output
+        output = [1] * len(nums)
+        prefix = 1
         for i in range(len(nums)):
-            if i == 0:
-                output.append(postfix_array[1])
-            elif i == len(nums)-1:
-                output.append(prefix_array[i- 1])
-            else:
-                output.append(prefix_array[i-1] * postfix_array[i+1])
-        return output
+            output[i] = prefix
+            prefix *= nums[i]
                 
+        postfix = 1
+        for j in range(len(nums) - 1,-1,-1):
+            output[j] *= postfix
+            postfix *= nums[j]
+            
+        return output
