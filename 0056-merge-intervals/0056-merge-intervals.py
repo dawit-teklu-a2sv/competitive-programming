f5c5a03a56ee07b91b1411b6bb5b20a51ea68077
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2022-10-19 16:56:16 +0300

    Time: 427 ms (5.28%), Space: 18.9 MB (10.21%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,16 +1,36 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals = sorted(intervals,key = lambda interval: interval[0])    
-        ans = []
-        i = 0
+#         intervals = sorted(intervals,key = lambda interval: interval[0])    
+#         ans = []
+#         i = 0
         
-        while i < len(intervals):
-            start = intervals[i][0]            
-            end = intervals[i][1]
-            i+= 1
+#         while i < len(intervals):
+#             start = intervals[i][0]            
+#             end = intervals[i][1]
+#             i+= 1
             
-            while i < len(intervals) and intervals[i][0] <= end:
-                end = max(end,intervals[i][1])
-                i += 1
-            ans.append([start,end])
-        return ans
+#             while i < len(intervals) and intervals[i][0] <= end:
+#                 end = max(end,intervals[i][1])
+#                 i += 1
+#             ans.append([start,end])
+#         return ans
+        intervals.sort()
+        stack = [intervals[0][0],intervals[0][1]]
+        
+        for i in range(len(intervals)):
+            if intervals[i][0] <= stack[-1]:
+                if intervals[i][1] <= stack[-1]:
+                    continue
+                else:
+                    stack.pop()
+            else:
+                stack.append(intervals[i][0])
+            stack.append(intervals[i][1])
+        output = []
+        for i in range(0,len(stack),2):
+            output.append([stack[i],stack[i+1]])
+        return output
+        print("stack ",stack)
+    
+    
+    
