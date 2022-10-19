7896d26b04b93a1328f0bf6410f1d39d533b7c9c
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2022-10-19 13:44:17 +0300

    Update CountingSort.py

--- CountingSort.py
+++ CountingSort.py
@@ -1,11 +1,6 @@
 def countingSort(arr):
     # Write your code here
-    # output = [0 for x in range(len(arr))]
-    n = len(arr)
     output = [0] * 100
-    for i,a in enumerate(arr):
-        if output[a] != 0:
-            output[a] += 1
-        else:
-            output[a] = 1
+    for item in arr:
+        outupt[item] += 1
     return output
