67db5bef403283bfe837bb5d65238574f1c3def0
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2022-10-19 15:12:15 +0300

    Time: 147 ms (63.59%), Space: 14.1 MB (9.18%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -1,23 +1,16 @@
 class Solution:
     def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        # sortedNums = sorted(nums,reverse=True)
-        # count = [0] * ( len(nums) + sortedNums[0])
-        # res = [0] * len(nums)
-        # for i in range(len(nums)):
-        #     count[nums[i]] += 1
-        # for j in range(1,len(count)):
-        #     count[j] += count[j-1]
-        # for i in range(len(res)):
-        #     if nums[i] == 0:
-        #         res[i] = 0
-        #     else:
-        #         res[i] = count[nums[i]-1]
-        # return res
-        temp = sorted(nums)
-        outputArray = [0]*len(nums)
-        n=0
-        for num in nums:
-            outputArray[n] = temp.index(num)
-            n+=1
-        return outputArray
+        sortedNums = sorted(nums,reverse=True)
+        count = [0] * ( len(nums) + sortedNums[0])
+        res = [0] * len(nums)
+        for i in range(len(nums)):
+            count[nums[i]] += 1
+        for j in range(1,len(count)):
+            count[j] += count[j-1]
+        for i in range(len(res)):
+            if nums[i] == 0:
+                res[i] = 0
+            else:
+                res[i] = count[nums[i]-1]
+        return res
         
