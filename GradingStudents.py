3e390c993c0ad62f4ba9b31880b168a56b7db754
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2022-10-19 10:23:52 +0300

    Update GradingStudents.py

--- GradingStudents.py
+++ GradingStudents.py
@@ -2,11 +2,8 @@ def gradingStudents(grades):
     # Write your code here
     output = []
     for index,grade in enumerate(grades):
-        next_multiple = (( grade // 5 ) + 1) * 5                        
-        if grade < 38:
+        if grade < 38 or grade % 5 < 3:
             output.append(grade)
-        elif next_multiple  - grade  < 3:
-            output.append(next_multiple)
-        else:
-            output.append(grade)
+        else: 
+            output.append(grade + 5 - (grade % 5 ))
     return output
