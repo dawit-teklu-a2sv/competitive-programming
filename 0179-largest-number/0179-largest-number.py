fd6ec7bb3f1063a19ec777f6727da36ddac56b14
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2022-10-19 17:24:46 +0300

    Time: 285 ms (5.02%), Space: 14 MB (23.85%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -5,4 +5,5 @@ class Solution:
             for j in range(len(nums) - 1):
                 if f'{nums[j]}{nums[j+1]}' < f'{nums[j+1]}{nums[j]}':
                     nums[j],nums[j+1] = nums[j+1],nums[j]
-        return str(int("".join(map(str,nums))))
+        res = "".join(map(str,nums))
+        return "0" if res[0] == "0" else res
