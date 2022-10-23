c93c339819e8ad10f76d594fac2c95abfa9c627f
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2022-10-23 23:22:40 +0300

    Time: 49 ms (74.48%), Space: 13.9 MB (9.38%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,9 +4,10 @@ class Solution:
     def fib(self, n: int) -> int:
         if n in self.previous_values:
             return self.previous_values[n]
-        if(n<=1):
-            return n
-        else:
-            self.previous_values[n] = self.fib(n-1) + self.fib(n-2)
-            return self.previous_values[n]
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        self.previous_values[n] = self.fib(n-1) + self.fib(n-2)
+        return self.previous_values[n]
         
