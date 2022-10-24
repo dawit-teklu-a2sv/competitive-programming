3b3ff735818b5d4573e8468ddd76aec294a75a15
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2022-10-24 12:42:30 +0300

    Time: 36 ms (96.74%), Space: 13.9 MB (75.44%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -1,20 +1,34 @@
 class Solution:
-      def findKthBit(self, n: int, k: int) -> str:
-        L = 2**n - 1
-        inv = 0
-        while (n > 1):
-            l = (L - 1) / 2
-            if (k == l + 1):
-                return '1' if inv % 2 == 0 else '0' # from added 1 in the middle
-            if (k > l + 1):
-                k -= l + 1
-                k = l + 1 - k # rev
-                inv += 1
+      def findKthBit(self, n: int, k: int) -> str:            
+            if n == 1:
+                return '0'
+            l = int(2 ** n - 1)
+            mid = l // 2
             
-            L = l
-            n -= 1
+            if k <= mid:
+                return self.findKthBit(n-1,k)
+            elif k == mid + 1:
+                return '1'
+            else:
+                k = l - k + 1
+                res = self.findKthBit(n-1,k)
+                print(f'res {res}')
+                return '0' if res != "0" else '1'
+#         L = 2**n - 1
+#         inv = 0
+#         while (n > 1):
+#             l = (L - 1) / 2
+#             if (k == l + 1):
+#                 return '1' if inv % 2 == 0 else '0' # from added 1 in the middle
+#             if (k > l + 1):
+#                 k -= l + 1
+#                 k = l + 1 - k # rev
+#                 inv += 1
+            
+#             L = l
+#             n -= 1
         
-        return '0' if inv % 2 == 0 else '1' # from the original 0
+#         return '0' if inv % 2 == 0 else '1' # from the original 0
 #     def findKthBit(self, n: int, k: int) -> str:
         
 #         def invert(s:str) -> str:
