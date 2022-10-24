397f6b29247017ebdaacbd2ab322dcfe8246373b
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2022-10-24 11:50:42 +0300

    Time: 65 ms (72.93%), Space: 13.9 MB (91.73%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -1,20 +1,36 @@
 class Solution:
-    def findKthBit(self, n: int, k: int) -> str:
+      def findKthBit(self, n: int, k: int) -> str:
+        L = 2**n - 1
+        inv = 0
+        while (n > 1):
+            l = (L - 1) / 2
+            if (k == l + 1):
+                return '1' if inv % 2 == 0 else '0' # from added 1 in the middle
+            if (k > l + 1):
+                k -= l + 1
+                k = l + 1 - k # rev
+                inv += 1
+            
+            L = l
+            n -= 1
         
-        def invert(s:str) -> str:
-            o = ""
-            for item in s:
-                if item == "0":
-                    o += '1'
-                else:
-                    o += '0'
-            return o
-        output = [''] * n;
+        return '0' if inv % 2 == 0 else '1' # from the original 0
+#     def findKthBit(self, n: int, k: int) -> str:
         
-        output[0] = '0'
-        print(output)
-        for i in range(1,n):
-            output[i] = output[i-1] + "1" + invert(output[i-1])[::-1]
+#         def invert(s:str) -> str:
+#             o = ""
+#             for item in s:
+#                 if item == "0":
+#                     o += '1'
+#                 else:
+#                     o += '0'
+#             return o
+#         output = [''] * n;
         
-        return output[n-1][k-1]
+#         output[0] = '0'
+#         print(output)
+#         for i in range(1,n):
+#             output[i] = output[i-1] + "1" + invert(output[i-1])[::-1]
+        
+#         return output[n-1][k-1]
         
