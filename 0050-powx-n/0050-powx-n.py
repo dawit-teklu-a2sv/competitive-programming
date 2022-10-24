3ff3f77e8d1f84e8c42b1cb8c91a6cb889a6c19c
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2022-10-24 10:43:16 +0300

    Time: 42 ms (78.21%), Space: 13.8 MB (68.17%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,18 +1,17 @@
-class Solution {
-    public double myPow(double x, int n) {
-        if(x == 0)
-            return 0;
-        if(n == 0)
-            return 1;
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
         
-        double p = myPow(x,n/2);
-        
-        if(n%2 == 0)
-            return  p * p;
-        else if(n%2 == 1)
-            return  p * p * x;
-        else
-            return 1/myPow(x,-n);
+        if n == 0:
+            return 1
+        if x == 0:
+            return 0
+        if n < 0:
+            return 1/self.myPow(x,-n)
+        p = self.myPow(x, n //2 )
+        if n %2 == 0:
+            return p * p
+        else:
+            return p * p * x
             
-    }
-}
+
+        
