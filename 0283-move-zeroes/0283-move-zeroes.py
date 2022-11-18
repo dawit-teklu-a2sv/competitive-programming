5313a34f84fac827f672a79b04ae87ed9451cf86
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2022-11-18 08:30:57 +0300

    Time: 444 ms (36.12%), Space: 15.7 MB (17.42%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,9 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left,right = 0,0
-        while right < len(nums):
+        left = 0
+        for right in range(len(nums)):
             if nums[right] != 0:
                 nums[left],nums[right] = nums[right],nums[left]
                 left += 1
-            right += 1
+        
