35632ba780f2dadab41d4766f6bdddc41b4cc2d6
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2022-11-20 11:48:00 +0300

    Time: 249 ms (65.71%), Space: 13.9 MB (93.78%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -5,12 +5,10 @@ class Solution:
         for idx in range(len(s)):
             arr[s[idx]] += 1
             largestCount = max(largestCount, arr[s[idx]])
-            print(f"idx:{idx}, maxLen: {maxlen}, largestCount: {largestCount} ")
             if maxlen - largestCount >= k:
                 arr[s[idx - maxlen]] -= 1
             else:
                 maxlen += 1
         return maxlen
     
-#AAAABBAABAA k = 2
 
