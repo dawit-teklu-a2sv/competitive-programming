957c181c80aaa74e23f78f3fd4af4f2c65f5c588
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2022-12-28 17:13:30 +0300

    Time: 32 ms (87.75%), Space: 13.8 MB (49.30%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.py
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -1,6 +1,6 @@
 class Solution:
     def smallestEvenMultiple(self, n: int) -> int:
         for i in range(2,2*n + 1):
-            if i % 2 == 0 and i %n == 0:
+            if i % 2 == 0 and i %n == 0:#checking if a number is both multiple of 2 and n 
                 return i
         
