2164f47b7c1588d0e3671493b0d8a1a93846ae08
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-01-02 22:26:30 +0300

    Time: 1354 ms (37.18%), Space: 43.4 MB (96.65%) - LeetHub

--- 2043-simple-bank-system/2043-simple-bank-system.py
+++ 2043-simple-bank-system/2043-simple-bank-system.py
@@ -5,7 +5,7 @@ class Bank:
         self.length = len(balance)
 
     def transfer(self, account1: int, account2: int, money: int) -> bool:
-        if account1 not in range(1,self.length+1) or account2 not in range(1,self.length+1) or money > self.balance[account1-1]:
+        if account1 not in range(1,self.length+1) or account2 not in range(1,self.length+1) or money > self.balance[account1-1]:#checking valid account or not and sufficient funds
             return False
         self.balance[account1-1] -= money
         self.balance[account2-1] += money
@@ -21,7 +21,7 @@ class Bank:
         
 
     def withdraw(self, account: int, money: int) -> bool:
-        if account not in range(1,len(self.balance)+1) or self.balance[account-1] < money:
+        if account not in range(1,len(self.balance)+1) or self.balance[account-1] < money:#check if the account is valid or not and have sufficient funds
             return False
         self.balance[account-1] -= money
         return True
