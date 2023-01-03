7b4398abf8a7cd1f97965f1f770912f759a61e09
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-01-03 17:32:28 +0300

    Time: 350 ms (6.92%), Space: 14.2 MB (8.30%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -1,5 +1,9 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
+        #since all items are less thatn the length of input arrays
+        # we can take values as index and append array[item] to the output array
+        # time complexity O(n)
+        # space complexity O(1)
         output = []
         for item in nums:
             output.append(nums[item])
