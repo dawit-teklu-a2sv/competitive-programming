02976acbcfe9a489e636a1d928cf2c14eaa9739b
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-01-04 21:22:58 +0300

    Time: 627 ms (8.34%), Space: 18.3 MB (8.47%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -4,20 +4,14 @@ class Solution:
         temp = defaultdict(list)
         for i in range(len(mat)):
             for j in range(len(mat[0])):
-                temp[i+j].append((j,mat[i][j]))
-        d = {}
+                temp[i+j].append(mat[i][j])
+        output = []   
+        print(temp)
         for key,value in temp.items():
-            d[key] = sorted(value,key=lambda x:x[0])
-        print(d)
-        output = []
-        for key,value in d.items():
-            if key % 2 == 1:
+            if key % 2 == 0:
                 for j in range(len(value)-1,-1,-1):
-                    output.append(value[j][1])
+                    output.append(value[j])
             else:
                 for j in range(len(value)):
-                    output.append(value[j][1])
+                    output.append(value[j])
         return output
-                
-                
-                
