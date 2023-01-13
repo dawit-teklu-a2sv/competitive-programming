42e537d7f210488e87fdb3e4e276e07b664fd6f4
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-01-13 14:38:32 +0300

    Time: 76 ms (52.36%), Space: 13.8 MB (94.69%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,5 +1,10 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
+        #count the occurence of every character using their ASCII value and put them on 2d array
+        #check if a character found in all words and mark the minimum occurence 
+        #append it to output array
+        #time complexity O(n*longestword length)
+        #space compleixty O(26 * n)
         character_map = [[0] * 26 for _ in range(len(words))]
         for  index,word in enumerate(words):
             for char in word:
