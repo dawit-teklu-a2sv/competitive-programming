fb4f50f946506b83ec81d22c5e6c691213e43f20
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-01-17 19:19:49 +0300

    Time: 623 ms (10.17%), Space: 14.3 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,14 +1,21 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        swaps = 0
+        # swaps = 0
+        # for i in range(len(heights)):
+        #     for j in range(len(heights)-i-1):
+        #         if heights[j] <  heights[j+1]:
+        #             heights[j],heights[j+1] = heights[j+1],heights[j]
+        #             names[j],names[j+1] = names[j+1],names[j]
+        #             swaps += 1
+        #     if swaps == 0:
+        #         break
         for i in range(len(heights)):
-            for j in range(len(heights)-i-1):
-                if heights[j] <  heights[j+1]:
-                    heights[j],heights[j+1] = heights[j+1],heights[j]
-                    names[j],names[j+1] = names[j+1],names[j]
-                    swaps += 1
-            if swaps == 0:
-                break
+            current_max_index = i
+            for j in range(i,len(heights)):
+                if heights[j]  > heights[current_max_index]:
+                    current_max_index = j
+            heights[i],heights[current_max_index] = heights[current_max_index],heights[i]
+            names[i],names[current_max_index] = names[current_max_index],names[i]
         return names
         
             
