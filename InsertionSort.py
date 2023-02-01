4826ea0d0def35d06be42541f3c02ae01055a43e
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-02-01 13:47:47 +0300

    Update InsertionSort.py

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,9 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'insertionSort1' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY arr
+#
+
 def insertionSort1(n, arr):
-    # Write your code here
-    last_element = arr[n-1]
-    for i in range(n-1,0,-1):
-        if last_element < arr[i-1]:
+    last_element = arr[-1]
+    
+    for i in range(n-1,-1,-1):
+        if arr[i-1] > last_element and i != 0:
             arr[i] = arr[i-1]
+            print(*arr)
         else:
             arr[i] = last_element
-        print(*arr,sep=' ')
+            print(*arr)
+            break 
+        
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    insertionSort1(n, arr)
