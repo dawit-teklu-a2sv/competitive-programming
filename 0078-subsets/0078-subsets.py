e3b628b405b0c4aa17a9581e015f877195817fba
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-03-10 09:44:40 +0300

    Time: 31 ms (88.27%), Space: 14.1 MB (77.75%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,8 +1,18 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        answer = [[]]
-        for item in nums:
-            newAnswer = answer[:]
-            for ans in newAnswer:
-                answer.append(ans + [item])
+        # answer = [[]]
+        # for item in nums:
+        #     newAnswer = answer[:]
+        #     for ans in newAnswer:
+        #         answer.append(ans + [item])
+        # return answer
+        answer = []
+        def backtrack(array,index,length):
+            if index == length:
+                answer.append(array)
+                return 
+            backtrack(array,index + 1,length)
+            backtrack(array + [nums[index]],index + 1,length)
+        backtrack([],0,len(nums))
         return answer
+                
