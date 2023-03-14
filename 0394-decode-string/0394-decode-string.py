d27bcd771e8c4e5357b4ab170c80af24936605c2
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-03-14 22:52:15 +0300

    Update 0394-decode-string.py

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -18,5 +18,28 @@ class Solution:
                     
                 stack.append(int(k) * temp)
         return "".join(stack)
+        def decodeString(self, s: str) -> str:
+ #recursion
+        
+#         def recur(s):
+#             sb = ""
+#             k = 0
+            
+#             while s:
+#                 c = s.popleft()
+#                 if c in string.ascii_letters:
+#                     sb += c
+#                 elif c in string.digits:
+#                     k = k * 10 + int(c)
+                    
+#                 elif c == '[':
+#                     temp_string = recur(s)
+#                     sb += temp_string * k
+#                     k = 0
+#                 else:
+#                     break 
+#             return sb
+            
+#         return recur(deque(s))
             
         
