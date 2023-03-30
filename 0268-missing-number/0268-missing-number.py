41d5259974f0ec9127109fa4be20069ee20274bf
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-03-30 11:15:01 +0300

    Time: 136 ms (65.75%), Space: 15.1 MB (66.06%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,15 +1,18 @@
 class Solution:
+    def calculateXor(self,n):
+        if n % 4 == 0:
+            return n
+        if n % 4 == 1:
+            return 1
+        if n % 4 == 2:
+            return n + 1
+        else:
+            return 0
     def missingNumber(self, nums: List[int]) -> int:
-        i = 0
         n = len(nums)
-        while i < n:
-            temp = nums[i]
-            if nums[i] == i or temp == n:
-                i += 1
-            else:
-                nums[i],nums[temp] = nums[temp],nums[i]
-        for i,item in enumerate(nums):
-            if i != item:
-               return i
-        return n
+        temp = self.calculateXor(n)
+        xor = 0
+        for i in nums:
+            xor ^= i
+        return xor ^ temp
         
