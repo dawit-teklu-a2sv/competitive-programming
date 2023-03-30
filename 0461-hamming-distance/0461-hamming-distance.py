083d3afb3c62755ab9c39bab495b47fdc537495f
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-03-30 17:48:15 +0300

    Time: 37 ms (21.86%), Space: 13.9 MB (47.87%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -0,0 +1,21 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        count = 0
+        while x > 0 and y > 0:
+            if x & 1 != y & 1:
+                count += 1
+            x >>= 1
+            y >>= 1
+        while x > 0:
+            if x & 1 == 1:
+                count += 1
+            x >>= 1
+        
+        while y > 0:
+            if y & 1 == 1:
+                count += 1
+            y >>= 1
+        return count
+        
+            
+        
