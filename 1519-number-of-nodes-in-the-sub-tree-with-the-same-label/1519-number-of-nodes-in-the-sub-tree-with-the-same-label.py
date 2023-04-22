16ac7891da74f2e30e9c03e826ac4030af039ec1
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-04-22 11:00:18 +0300

    Time: 2191 ms (67.51%), Space: 164 MB (95.24%) - LeetHub

--- 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.py
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.py
@@ -1,23 +1,25 @@
 class Solution:
     def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
-        adjL = [[]for _ in range(n)]
-        labels = [label for label in labels]
-        count = [0] * n
+        adjL = [[] for _ in range(n)]
+        
         for src,dst in edges:
             adjL[src].append(dst)
             adjL[dst].append(src)
-
+            
+        count = [0] * n 
         
-        def dfs(index,adjL,labels,count,parent):
+        def dfs(index,parent):
             labelCount = defaultdict(int)
+            
             for nbr in adjL[index]:
-                if parent == nbr:
+                if nbr == parent:
                     continue
-                childLabelCount = dfs(nbr,adjL,labels,count,index)
-                for label in childLabelCount.keys():
-                    labelCount[label] += childLabelCount[label]
+                childLabelCount  = dfs(nbr,index)
+                for key,value in childLabelCount.items():
+                    labelCount[key] += value
             labelCount[labels[index]] += 1
             count[index] = labelCount[labels[index]]
             return labelCount
-        dfs(0,adjL,labels,count,0)
+        dfs(0,0)
         return count
+                    
