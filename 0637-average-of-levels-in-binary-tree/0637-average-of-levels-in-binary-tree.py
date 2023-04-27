507d922fba1582b0531543b13b7594b91b16419a
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-04-27 17:34:49 +0300

    Time: 69 ms (5.79%), Space: 18.9 MB (7.72%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
@@ -6,18 +6,35 @@
 #         self.right = right
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-        graph = defaultdict(list)
+        queue = deque()
         
-        def levelOrder(node,level):
-            if not node:
-                return 
-            graph[level].append(node.val)
-            levelOrder(node.left,level+1)
-            levelOrder(node.right,level+1)
-        levelOrder(root,0)
-        output = []
-        for key,value in graph.items():
-            output.append(sum(value)/len(value))
-        return output
+        queue.append(root)
+        ans = []
+        while queue:
+            length = len(queue)
+            total = 0
+            for _ in range(length):
+                temp = queue.popleft()
+                total += temp.val
+                
+                if temp.left:
+                    queue.append(temp.left)
+                if temp.right:
+                    queue.append(temp.right)
+            ans.append(total/length)
+        return ans
+#         graph = defaultdict(list)
+        
+#         def levelOrder(node,level):
+#             if not node:
+#                 return 
+#             graph[level].append(node.val)
+#             levelOrder(node.left,level+1)
+#             levelOrder(node.right,level+1)
+#         levelOrder(root,0)
+#         output = []
+#         for key,value in graph.items():
+#             output.append(sum(value)/len(value))
+#         return output
         
     
