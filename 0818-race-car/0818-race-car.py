2cf95aff720910479f55c124315b6db18ad1fa9c
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-04-28 19:10:11 +0300

    Time: 49 ms (63.70%), Space: 16.9 MB (28.55%) - LeetHub

--- 0818-race-car/0818-race-car.py
+++ 0818-race-car/0818-race-car.py
@@ -1,20 +1,19 @@
 class Solution:
     def racecar(self, target: int) -> int:
-        queue = deque([(0,0,1)])
+        queue = deque([[0,0,1]])
         
-        visited = set()
+        visited = set((0,1))
         
         while queue:
             moves,position,speed = queue.popleft()
+            
             if position == target:
                 return moves
+            
             if (position,speed) not in visited:
-                queue.append((moves + 1, position + speed, speed * 2))
+                queue.append([moves + 1, position + speed, speed * 2])
                 
-                if ((position + speed > target and speed > 0) or 
-                (position + speed < target and speed < 0)):
+                if((position + speed > target and speed > 0) or
+                   (position + speed < target and speed < 0)):
                     speed = -1 if speed > 0 else 1
-                    queue.append((moves + 1, position,speed))
-                
-                    
-        
+                    queue.append([moves + 1, position,speed])
