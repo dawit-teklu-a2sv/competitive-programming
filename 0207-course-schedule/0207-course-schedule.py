7c1d05f3177a0c51b42384912c263e1a5a7d7929
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-05-12 19:24:25 +0300

    Time: 101 ms (63.72%), Space: 17.9 MB (23.17%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,26 +1,51 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        colors = [0 for _ in range(numCourses)]
+        #BFS Implementation
         graph = [[] for _ in range(numCourses)]
-        for c, p in prerequisites:
-            graph[c].append(p)
-            
-        def dfs(node):
-            if colors[node] == 1:
-                return False
-            
-            colors[node] = 1
-            
-            for p in graph[node]:
-                if colors[p] == 2:
-                    continue
-                if not dfs(p):
-                    return False
-            colors[node] = 2
-            return True
+        incoming = [0 for _ in range(numCourses)]
         
-        for i in range(numCourses):
-            if not dfs(i):
-                return False
-        return True
+        for c,p in prerequisites:
+            graph[p].append(c)
+            incoming[c] += 1
+        queue = deque()
+        order = []
+        for i,indegree in enumerate(incoming):
+            if not indegree:
+                queue.append(i)
+        print(queue)       
+        while queue:
+            f = queue.popleft()
+            order.append(f)
+            
+            for n in graph[f]:
+                incoming[n] -= 1
+                if not incoming[n]:
+                    queue.append(n)
+                    
+        return len(order) == numCourses
+        
+        # dfs implementaion
+#         colors = [0 for _ in range(numCourses)]
+#         graph = [[] for _ in range(numCourses)]
+#         for p, c in prerequisites:
+#             graph[c].append(p)
+            
+#         def dfs(node):
+#             if colors[node] == 1:
+#                 return False
+            
+#             colors[node] = 1
+            
+#             for p in graph[node]:
+#                 if colors[p] == 2:
+#                     continue
+#                 if not dfs(p):
+#                     return False
+#             colors[node] = 2
+#             return True
+        
+#         for i in range(numCourses):
+#             if not dfs(i):
+#                 return False
+#         return True
         
