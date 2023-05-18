4f49f8ef73e173d9f099d04cc72db4068f1f4e18
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-05-18 17:57:13 +0300

    Time: 2044 ms (51.34%), Space: 107.3 MB (56.73%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,22 +1,54 @@
+class UnionFind:
+    def __init__(self,size):
+        self.root = [i for i in range(size)]
+        self.size = [0] * size
+    
+    
+    def find(self,x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    
+    def union(self,x,y):
+        xparent = self.find(x)
+        yparent = self.find(y)
+        xsize = self.size[x]
+        ysize = self.size[y]
+        
+        if xparent != yparent:
+            if xsize > ysize:
+                self.root[yparent] = xparent 
+            elif ysize > xsize:
+                self.root[xparent] = yparent 
+            else:
+                self.root[yparent] = xparent
+                self.size[xparent] += 1
+            
+    
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        
-        visited = [False for _ in range(n)]
-        
-        graph = defaultdict(list)
-        
+        findUnion = UnionFind(n)
         for u,v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
-            
-        queue = deque([source])
+            findUnion.union(u,v)
+        return findUnion.find(source) == findUnion.find(destination)
         
-        while queue:
-            curr = queue.popleft()
-            if curr == destination:
-                return True
+#         visited = [False for _ in range(n)]
+        
+#         graph = defaultdict(list)
+        
+#         for u,v in edges:
+#             graph[u].append(v)
+#             graph[v].append(u)
             
-            if curr in graph and not visited[curr]:
-                queue.extend(graph[curr])
-            visited[curr] = True
-        return False
+#         queue = deque([source])
+        
+#         while queue:
+#             curr = queue.popleft()
+#             if curr == destination:
+#                 return True
+            
+#             if curr in graph and not visited[curr]:
+#                 queue.extend(graph[curr])
+#             visited[curr] = True
+#         return False
