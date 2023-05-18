27483544bb49845b19f29a6b5acde1177aa88525
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-05-18 19:33:57 +0300

    Time: 71 ms (33.24%), Space: 16.9 MB (19.61%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,28 +1,50 @@
+class UnionFind:
+    def __init__(self,size):
+        self.root = [i for i in range(size + 1)]
+        self.size = [1] * (size + 1)
+    
+    
+    def find(self,x):
+        if x == self.root[x]:
+            return x
+        parent = self.find(self.root[x])
+        self.root[x] = parent
+        return self.root[x]
+    
+    def union(self,x,y):
+        xparent = self.find(x)
+        yparent = self.find(y)
+        xsize = self.size[x]
+        ysize = self.size[y]
+        if xparent != yparent:
+            if xsize > ysize:
+                self.root[yparent] = xparent 
+            elif ysize > xsize:
+                self.root[xparent] = yparent 
+            else:
+                self.root[yparent] = xparent
+                self.size[xparent] += 1
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        graph = defaultdict(set)
+#         graph = defaultdict(set)
         
-        def dfs(source,target):
-            if source not in visited:
-                visited.add(source)
-                if source == target:
-                    return True
+#         def dfs(source,target):
+#             if source not in visited:
+#                 visited.add(source)
+#                 if source == target:
+#                     return True
+
                 
-                # res =  [dfs(n,target) for n in graph[source]]
-                # return any(res)
-                for n in graph[source]:
-                    if dfs(n,target):
-                        return True
-                # for nei in graph[source]:
-                #     print(f"nei: {nei} source {source}")
-                #     return dfs(nei,target)
-        
+#                 for n in graph[source]:
+#                     if dfs(n,target):
+#                         return True
+
+        unionFind = UnionFind(len(edges))
         for u,v in edges:
             visited = set()
-            if u in graph and v in graph and dfs(u,v):
+            if unionFind.find(u) == unionFind.find(v):
                 return [u,v]
-            graph[u].add(v)
-            graph[v].add(u)
+            unionFind.union(u,v)
                 
             
                 
