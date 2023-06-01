86e64a30dcfe331b700353987c7ab3268fa572a4
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-06-01 19:17:49 +0300

    Time: 52 ms (18.76%), Space: 16.5 MB (17.00%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,30 +1,31 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        #bottom-up
-        n = len(nums)
-        if n == 1:
-            return nums[0]
-        dp = [0] * n
-        dp[0],dp[1] = nums[0],max(nums[0],nums[1])
+#         #bottom-up
+#         n = len(nums)
+#         if n == 1:
+#             return nums[0]
+#         dp = [0] * n
+#         dp[0],dp[1] = nums[0],max(nums[0],nums[1])
         
-        for i in range(2,n):
-            dp[i] = max(dp[i-1],nums[i] + dp[i-2])
+#         for i in range(2,n):
+#             dp[i] = max(dp[i-1],nums[i] + dp[i-2])
         
-        return dp[n-1]
+#         return dp[n-1]
         
         #top-bottom
-#         n = len(nums)
-#         memo = {}
-        
-#         def solve(i):
-#             if i == 0:
-#                 return nums[0]
-#             if i == 1:
-#                 return max(nums[0],nums[1])
+        n = len(nums)
+        memo = {}
+        @cache
+        def solve(i):
+            if i == 0:
+                return nums[0]
+            if i == 1:
+                return max(nums[0],nums[1])
             
-#             if i not in memo:
-#                 memo[i] = max(solve(i-1),nums[i] + solve(i-2))
-#             return memo[i]
-#         return solve(n-1)
+            # if i not in memo:
+            #     memo[i] = max(solve(i-1),nums[i] + solve(i-2))
+            # return memo[i]
+            return max(solve(i-1),nums[i] + solve(i-2))
+        return solve(n-1)
                 
         
