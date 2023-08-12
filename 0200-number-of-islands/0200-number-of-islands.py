045c260d6764c24b8580831f3e195156d987601d
Dawit Teklu <58257925+Dawit2119@users.noreply.github.com>
2023-08-12 14:26:05 +0300

    Time: 289 ms (87.40%), Space: 19.2 MB (50.27%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,16 +3,16 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
         directions = [[-1,0],[1,0],[0,-1],[0,1]]
-        
+        visited = [[False] * n for _ in range(m)]
         def dfs(i,j):
-            if 0 <= i < m and 0 <= j < n and grid[i][j] == "1":
-                grid[i][j] = "0"
+            if 0 <= i < m and 0 <= j < n and not visited[i][j] and grid[i][j] == '1':
+                visited[i][j] = True
                 for k,l in directions:
                     dfs(i+k,j+l)
         count = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == '1':
+                if grid[i][j] == '1' and not visited[i][j]:
                     dfs(i,j)
                     count += 1
         return count
