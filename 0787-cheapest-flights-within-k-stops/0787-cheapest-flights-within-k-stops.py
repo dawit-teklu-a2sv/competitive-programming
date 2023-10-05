ba44983cfa54d554dd47a72ed262ba5fdfdd3173
Dawit Teklu <58257925+dawit-teklu-a2sv@users.noreply.github.com>
2023-10-05 04:09:48 -0700

    Time: 141 ms (47.64%), Space: 17.5 MB (75.68%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -23,40 +23,21 @@ class Solution:
                 
 #         return ans if ans != float('inf') else -1
 
-#         prices = [float('inf')] * n 
+        prices = [float('inf')] * n 
         
-#         prices[src] = 0 
-#         for i in range(k+1):
-#             temp_prices = prices
-            
-#             for source,distination,price in flights:
-#                 current_cheapest = temp_prices[distination]
-#                 new_price = prices[source] + price
-                
-#                 if new_price < current_cheapest:
-#                     temp_prices[distination] = new_price
-            
-#             prices = temp_prices 
-            
-#         return prices[dst] if prices[dst] != float('inf') else -1
-
-        prices = [float('inf')] * n
-        # set source price to 0 since it costs nothing to start at source
-        prices[src] = 0
-        
-        for i in range(k + 1):
-            # go through all edges
+        prices[src] = 0 
+        for i in range(k+1):
             temp_prices = list(prices)
-            for source, dest, price in flights:
-                current_cheapest = temp_prices[dest]
+            
+            for source,distination,price in flights:
+                current_cheapest = temp_prices[distination]
                 new_price = prices[source] + price
                 
-                # update price if it is cheaper
-                # new price is the cheapest price so far it costs to get to source + price to get to dest from source
                 if new_price < current_cheapest:
-                    temp_prices[dest] = new_price
-            prices = temp_prices
-        
+                    temp_prices[distination] = new_price
+            
+            prices = temp_prices 
+            
         return prices[dst] if prices[dst] != float('inf') else -1
     
 
