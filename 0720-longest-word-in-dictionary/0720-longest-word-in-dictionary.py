75c2c4d98cb6aacadb8b669738ac2d1bad9f3273
Dawit Teklu <58257925+dawit-teklu-a2sv@users.noreply.github.com>
2023-10-08 14:30:40 +0300

    Trie Implementation

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -1,3 +1,4 @@
+#Using trie 
 class Solution:
     def longestWord(self, words: List[str]) -> str:
         
@@ -9,4 +10,51 @@ class Solution:
                 if(all(word[:i] in words_set for i in range(1,len(word)))):
                     ans = word
         return ans
+
+class Node:
+    
+    def __init__(self):
+        self.children = {}
+        self.isEnd = False
+        
+class Trie:
+    
+    def __init__(self):
+        self.root = Node()
+        
+    def insert(self,word:str):
+        root = self.root 
+        
+        for ch in word:
+            if ch not in root.children:
+                root.children[ch] = Node()
+            root = root.children[ch]
+        root.isEnd = True
+    def checkAllPrefixes(self,word):
+        root = self.root 
+        for ch in word:
+            if ch in root.children:
+                root = root.children[ch]
+                if not root.isEnd:
+                    return False
+            else:
+                return False 
+        return True
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        trie = Trie()
+        for word in words:
+            trie.insert(word)
+        ans = ""
+        
+        for word in words:
+            if trie.checkAllPrefixes(word):
+                if len(word) > len(ans):
+                    ans = word
+                elif len(word) == len(ans) and word < ans:
+                    ans = word 
+        return ans
+            
+        
+        
     
