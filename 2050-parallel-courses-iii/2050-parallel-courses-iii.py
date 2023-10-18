c3b63dc5c886ead587a9132473c4f1a51ed0e5a4
Dawit Teklu <58257925+dawit-teklu-a2sv@users.noreply.github.com>
2023-10-18 14:32:10 +0300

    Time: 1464 ms (45.29%), Space: 134.5 MB (5.00%) - LeetHub

---
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
+        
+        graph = [[] for _ in range(n+1)]
+        
+        
+        for u,v in relations:
+            graph[u].append(v)
+            
+        dp = [0] * (n + 1)
+        def dfs(u):
+            if dp[u]:
+                return dp[u]
+            
+            dp[u] = max((dfs(v) for v in graph[u]),default=0) + time[u-1]
+            
+            return dp[u]
+            
+        for i in range(1,n+1):
+            dfs(i)
+            
+        return max(dp)
